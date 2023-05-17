78026194f01320d4418b70bb5472d8c66679b60d
Filip Larsson <hockeyfilip12@gmail.com>
2023-05-17 16:37:35 +0200

    3rd commit

--- task2.py
+++ task2.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
+def calculate_w_prior():
+    w_dimension = 3
+    alpha = 0.2
+    mean = np.zeros(w_dimension)
+    covariance = np.eye(w_dimension) / alpha
+    prior_w = multivariate_normal(mean, covariance)
+    return prior_w
+
+
 def generate_input_space():
     X1 = np.arange(-1, 1.05, 0.05)
     X2 = np.arange(-1, 1.05, 0.05)
@@ -26,6 +35,21 @@ def generate_seen_data(variance):
     T_values.reverse()
     return input_matrix, T_values
 
+def generate_unseen_data(variance):
+    T_values = []
+    X1_first_half = np.arange(-1.00, -0.30, 0.05)
+    X1_second_half = np.arange(0.35, 1.05, 0.05)
+    X2_first_half = np.arange(-1.00, -0.30, 0.05)
+    X2_second_half = np.arange(0.35, 1.05, 0.05)
+    X1 = np.concatenate(X1_first_half, X1_second_half)
+    X2 = np.concatenate(X2_first_half, X2_second_half)
+
+    input_matrix = np.array([[x1, x2] for x1 in X1 for x2 in X2])
+    for X in input_matrix:
+        t = calculate_t(X[0], X[1], variance)
+        T_values.append(t)
+    T_values.reverse()
+    return input_matrix, T_values
 
 def main():
     variance = 0.2
