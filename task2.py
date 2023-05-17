3492429495d650fb0fde8abcf73ca1f7ea6f3e4d
Filip Larsson <hockeyfilip12@gmail.com>
2023-05-17 16:12:22 +0200

    2nd commit

--- task2.py
+++ task2.py
@@ -15,18 +15,36 @@ def calculate_t(x1, x2, variance):
     t = w[0] + w[1]*x1 + w[2]*x2 + epsilon.rvs()
     return t
 
+def generate_seen_data(variance):
+    T_values = []
+    X1 = np.arange(-0.3, 0.35, 0.05)
+    X2 = np.arange(-0.3, 0.35, 0.05)
+    input_matrix = np.array([[x1, x2] for x1 in X1 for x2 in X2])
+    for X in input_matrix:
+        t = calculate_t(X[0], X[1], variance)
+        T_values.append(t)
+    T_values.reverse()
+    return input_matrix, T_values
+
+
 def main():
     variance = 0.2
+
+    # Uppgift 1:
+    '''
     input_matrix = generate_input_space()
     T_values = []
     for X in input_matrix:
         t = calculate_t(X[0], X[1], variance)
         T_values.append(t)
     T_values.reverse()
+    '''
+    input_matrix, T_values = generate_seen_data(variance)
 
     X1 = input_matrix[:, 0]
     X2 = input_matrix[:, 1]
     T_values = np.array(T_values)
+    
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
